034da7d0e239a709f958b33d690571793f2d5b92
cpopesc <cpopesc@gmail.com>
2014-08-07 15:16:41 -0400

    Cristian Popescu - R Programming - Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+# R Programming - Assignment 2 - cacheSolve function
+# by Cristian Popescu (August 6th, 2014)
+#
+#
 
-## Write a short comment describing this function
+#this function will create the basic operations to apply to a matrix object
+#it will store in invMatrix the inverse matrix computed
+makeCacheMatrix <- function(x=numeric(), numrow, numcol) {
+  #we convert the vector into a matrix
+  myMatrix <- matrix(data = x, nrow=numrow, ncol=numcol, byrow=FALSE, dimnames=NULL)
+  invMatrix <- NULL
+  
+  #set the current matrix and resets the inverse matrix previously computed
+  set <- function(y) {
+    myMatrix <<- y
+    invMatrix <<- NULL
+  }
+  
+  #get current matrix
+  get <- function() myMatrix
+  
+  #stores the inverse matrix
+  setInverse <- function(myInvMatrix) invMatrix <<- myInvMatrix
+  
+  #retrieves the inverse matrix
+  getInverse <- function() invMatrix
+  
 
-makeCacheMatrix <- function(x = matrix()) {
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
 
+#it generates the inverse matrix       
+cacheSolve <- function(x, ...){
+  myInv <- x$getInverse()
+
+ #if inverse matrix was already computed, then returns it
+  if(!is.null(myInv)) {
+    message("getting cached data")
+    return(myInv)
+  }
+  
+  myData <- x$get()
+
+  #computes the inverse matrix using solve
+  myInv <- solve(myData)
+
+  #stores the inverse matrix
+  x$setInverse(myInv)
+
+  print(x$getInverse())
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+mdat <- makeCacheMatrix(c(1,1,2,1,2,2,1,1,1),3,3)
+cacheSolve(mdat)
